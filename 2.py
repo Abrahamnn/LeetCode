23729b748dd06510438db7fa4068e16c0a6fb26d
HGYearn <hgyearn@163.com>
2018-04-25 12:03:32 +0800

    两数相加

---
+++ 2.py
@@ -0,0 +1,31 @@
+# 两数相加
+# 给定两个非空链表来表示两个非负整数。位数按照逆序方式存储，它们的每个节点只存储单个数字。将两数相加返回一个新的链表
+示例： 
+  输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+  输出：7 -> 0 -> 8
+  原因：342 + 465 = 807
+  
+ class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = ListNode(0)
+        current = head
+        carry = 0
+        while l1 != None or l2 != None or carry != 0:
+            if l1 != None:
+                carry += l1.val
+                l1 = l1.next
+            if l2 != None:
+                carry += l2.val
+                l2 = l2.next
+            node = ListNode(carry%10)
+            carry = int(carry/10)
+            current.next = node
+            current = node
+        return head.next
+        
+        #  代码太长 很冗余
