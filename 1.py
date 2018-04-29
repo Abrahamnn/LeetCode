d69140ca91f4b1dc82f9346d6c1d3c1cc217cc6c
HGYearn <hgyearn@163.com>
2018-04-29 14:08:32 +0800

    两数之和
    多看看大佬的

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 # 题目：给定一个整数数组和一个目标值，找出数组中和为目标值的两个数
-使用内置函数  enumerate 比较好
+# 使用内置函数  enumerate 比较好  （大佬的东西）
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -11,3 +11,6 @@ class Solution(object):
             for index2, num2 in enumerate(nums[index1+1:]):
                 if num1 + num2 == target:
                     return [index1, index1+index2+1]
+                
+                
+                
